d0f0626322897a82b8fb513dfe5dfed0a50c00a1
junh4533 <junh4533@gmail.com>
2019-07-30 16:48:54 -0400

    add 2 more Tvs

--- video_upload/forms.py
+++ video_upload/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Document
 # from crispy_forms.helper import FormHelper
-# from crispy_forms.layout import Submit, Layout, Field
+# from crispy_forms.layout import Layout, Submit, Row
 # from crispy_forms.bootstrap import (
 #     PrependedText, PrependedAppendedText, FormActions)
 
--- video_upload/views.py
+++ video_upload/views.py
@@ -4,36 +4,39 @@ import os
 from django.views.generic import CreateView
 from .forms import DocumentForm
 from .models import Document
+from django.contrib.auth.decorators import login_required
 import time
 
+
 # Create your views here.
+
 def index(request):
-    # if request.is_ajax():
-    #     #do something
-    #     request_data = request.POST
-    #     print("ok")
+    hostname = "10.30.125.20" #long shing
+    hostname2 = "10.30.125.125" #york
+    hostname3 = "10.30.125.30" #ashley
+    hostname4 = "0.0.0.0" #me
+
     if request.method == 'GET':
         #ping
-        hostname = "10.30.126.22" #long shing's IPv4
-        hostname2 = "10.30.125.125"
         response = os.system("ping " + hostname +  " -n 1 -w 1") # ping once at hostname
-        response2 = os.system("ping " + hostname2 +  " -n 1 -w 1") # ping once at hostname
-        
+        response2 = os.system("ping " + hostname2 +  " -n 1 -w 1")
+        response3 = os.system("ping " + hostname3 +  " -n 1 -w 1")
+        response4 = os.system("ping " + hostname4 +  " -n 1 -w 1")
+        print(response, response2, response3, response4)
         form = DocumentForm()
         args = {
             "form": form,
             "response": response,
-            "response2": response2
+            "response2": response2,
+            "response3": response3,
+            "response4": response4
         }
         return render(request, 'video_upload/index.html', args)
-        # form = DocumentForm()
-        # return render(request, 'video_upload/index.html', {
-        #     'form': form
-        # })
         
     if request.method == 'POST':
+        
         #ping
-        hostname = "10.30.124.80" #long shing's IPv4
+        hostname = "10.30.125.20"
         response = os.system("ping " + hostname +  " -n 1 -w 1") # ping once at hostname
         
         #check ping response
@@ -42,10 +45,11 @@ def index(request):
             form = DocumentForm(request.POST, request.FILES)
             if form.is_valid(): 
                 form.save()
+                
                 return render(request, 'video_upload/success.html')
         else:
             print(hostname, 'is down!')
-            return HttpResponse("<h1>Selected TV is not online</h1>")
+            return render(request, 'video_upload/failed.html')
     # else:
     #     form = DocumentForm()
     
